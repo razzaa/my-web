51c83bbaea82e0d6db1931274961e4a3073902fe
codewithali <ali-raza@ashlarglobal.com>
2021-04-28 22:45:54 +0500

    nav design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('layouts.app')
+    <title>My Web</title>
+</head>
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+</body>
+    <script src="{{ mix('js/app.js') }}"></script>
+</html>
